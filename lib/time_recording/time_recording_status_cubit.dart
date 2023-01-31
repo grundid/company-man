3cf798e8fcff11d80a828817c815ea44a267ee33
Adrian Stabiszewski <as@grundid.de>
2023-01-31 12:24:25 +0100

    cleanup log #32

--- lib/time_recording/time_recording_status_cubit.dart
+++ lib/time_recording/time_recording_status_cubit.dart
@@ -14,7 +14,6 @@ class TimeRecordingStatusCubit extends Cubit<AppState> {
   TimeRecordingStatusCubit() : super(InProgress());
 
   update(WorkTimeState workTimeState) {
-    print(workTimeState);
     if (workTimeState.to != null) {
       Duration duration = workTimeState.workDuration!;
       Duration pauseDuration = workTimeState.pauses.fold(
